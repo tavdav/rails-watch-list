9f0e4abde87648cead15ca08e4a95a95d09cad7c
Tavis David <tavis.david@airthings.com>
2021-05-08 14:41:01 +0200

    movie api added

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
-  validates :name, uniqueness: true, presence: true
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+
+  validates :name, uniqueness: true, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+movies['results'].each do |movie|
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: movie['backdrop_path'],
+    rating: movie['vote_average']
+  )
+end
